95deb374b838376ad4686f49d1d43c35d18712f3
Cristian Gorrino <cristiangorrino@Cristians-MacBook-Air.local>
2015-12-07 11:26:22 -0800

    added header with styles and homepage gif

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<!--header include-->
+		<header class="cf">
+			<a href="#" class="logo"><img src="img/logo-main.jpg" alt="Cuadra Urbano"></a>
+			<nav>
+				<ul>
+					<li><a href="">Cuadra</a></li>
+					<li><a href="">Servicios</a></li>
+					<li><a href="">Proyectos</a></li>
+					<li><a href="">Contacto</a></li>
+				</ul>
+			</nav>
+		</header>
+<!--header include-->
