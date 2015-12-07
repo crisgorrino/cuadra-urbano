c7445ed5f05c88c1644b021b91c29aed47d8455f
Cristian Gorrino <cristiangorrino@Cristians-MacBook-Air.local>
2015-12-07 13:32:18 -0800

    added all pages with header

---
+++ servicios.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+  <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Cuadra Urbano - Servicios</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="styles/main.css">
+
+    </head>
+    <body>
+		<!--header include-->
+		<?php include('includes/header.php') ?>
+		<!--header include-->
+		
+		<div class="mast">
+			<img src="img/gifs/servicios.gif" alt="cuadra urbano">
+		</div>
+		
+    </body>
+</html>
