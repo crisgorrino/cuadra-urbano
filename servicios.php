e70ef94b95a8e9367893bcdd0a72050878fdd82b
Cristian Gorrino <cristiangorrino@Cristians-MacBook-Air.local>
2015-12-16 14:18:19 -0600

    servicios final faltan contenidos

--- servicios.php
+++ servicios.php
@@ -30,6 +30,7 @@
 					    <li class='tab'><a href="#pp">PP</a></li>
 					  </ul>
 					  <div class="clear"></div>
+					  <!--US-->
 					  <div id="us" class="cf">
 					    <h2>Urbanismo Social</h2>
 					    
@@ -86,24 +87,243 @@ En Cuadra Urbanismo diseñamos plataformas de co-creación a partir de la sensib
 							
 					    <!-- content -->
 					  </div>
+					  <!--US-->
+					  
+					  <!--UI-->
 					  <div id="ui">
-					    <h2>UI</h2>
+					    <h2>U I</h2>
+					    
+					    	<h5 class="border-top-red">
+						    La ciudad es producto de una construcción social. Desde esta perspectiva buscamos promover el enriquecimiento del capital social a través de intervenciones comunitarias que contribuyan a la mejora del entorno y el fortalecimiento de la identidad.
+					    	</h5>
+					    	
+					    	<p>Todo proyecto urbano de alto impacto social debe contemplar la participación activa de su comunidad.<br>
+					    	<br>
+En Cuadra Urbanismo diseñamos plataformas de co-creación a partir de la sensibilización social y la aplicación de métodos etnográficos. El objetivo es acercar a los tomadores de decisiones con los ciudadanos y lograr así procesos democráticos de transformación urbana.</p>
+
+							<ul>
+								<li><span class="red-square"></span> Plataformas de co-creación de    proyectos (socialización)</li>
+								<li><span class="red-square"></span> Activaciones urbanas     (placemaking)</li>
+								<li><span class="red-square"></span> Estrategias de sensibilización y  comunicación de  proyectos urbanos</li>
+								<li><span class="red-square"></span> Rehabilitación colaborativa de barrios (Huertos urbanos, murales, gestión cultural, etc.)</li>
+							</ul>	
+							
+							<div class="clear"></div>									
+							<!--proyectos-->
+							<div class="serv-proyectos">
+								
+								<!--proy 1-->
+								<div class="sp center">
+									<img src="img/proy-placeholder.jpg" alt="">
+									<h2>Titulo 01</h2>
+									<p>This is Photoshop's version  of Lorem Ipsum velit auctor aliquet. Aenean sollicitudin lorem quis bibendum auctor, nisi elit 											consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan 										ipsum velit.										
+									</p>
+								</div>
+								<!--proy 2-->
+								
+								<!--proy 1-->
+								<div class="sp center">
+									<img src="img/proy-placeholder.jpg" alt="">
+									<h2>Titulo 02</h2>
+									<p>This is Photoshop's version  of Lorem Ipsum velit auctor aliquet. Aenean sollicitudin lorem quis bibendum auctor, nisi elit 											consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan 										ipsum velit.										
+									</p>
+								</div>
+								<!--proy 1-->
+								
+								
+								<!--proy 1-->
+								<div class="sp center">
+									<img src="img/proy-placeholder.jpg" alt="">
+									<h2>Titulo 03</h2>
+									<p>This is Photoshop's version  of Lorem Ipsum velit auctor aliquet. Aenean sollicitudin lorem quis bibendum auctor, nisi elit 											consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan 										ipsum velit.										
+									</p>
+								</div>
+								<!--proy 1-->
+								
+							</div>	
+							<!--proyectos-->									
+							
+							
 					    <!-- content -->
 					  </div>
+					  <!--UI-->
+					  
+					  <!--TIC-->
 					  <div id="tic">
-					    <h2>TIC</h2>
-					    <!-- content -->
-					  </div>
+					    <h2>T I C</h2>
+					    
+					    	<h5 class="border-top-red">
+						    La ciudad es producto de una construcción social. Desde esta perspectiva buscamos promover el enriquecimiento del capital social a través de intervenciones comunitarias que contribuyan a la mejora del entorno y el fortalecimiento de la identidad.
+					    	</h5>
+					    	
+					    	<p>Todo proyecto urbano de alto impacto social debe contemplar la participación activa de su comunidad.<br>
+					    	<br>
+En Cuadra Urbanismo diseñamos plataformas de co-creación a partir de la sensibilización social y la aplicación de métodos etnográficos. El objetivo es acercar a los tomadores de decisiones con los ciudadanos y lograr así procesos democráticos de transformación urbana.</p>
+
+							<ul>
+								<li><span class="red-square"></span> Plataformas de co-creación de    proyectos (socialización)</li>
+								<li><span class="red-square"></span> Activaciones urbanas     (placemaking)</li>
+								<li><span class="red-square"></span> Estrategias de sensibilización y  comunicación de  proyectos urbanos</li>
+								<li><span class="red-square"></span> Rehabilitación colaborativa de barrios (Huertos urbanos, murales, gestión cultural, etc.)</li>
+							</ul>	
+							
+							<div class="clear"></div>									
+							<!--proyectos-->
+							<div class="serv-proyectos">
+								
+								<!--proy 1-->
+								<div class="sp center">
+									<img src="img/proy-placeholder.jpg" alt="">
+									<h2>Titulo 01</h2>
+									<p>This is Photoshop's version  of Lorem Ipsum velit auctor aliquet. Aenean sollicitudin lorem quis bibendum auctor, nisi elit 											consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan 										ipsum velit.										
+									</p>
+								</div>
+								<!--proy 2-->
+								
+								<!--proy 1-->
+								<div class="sp center">
+									<img src="img/proy-placeholder.jpg" alt="">
+									<h2>Titulo 02</h2>
+									<p>This is Photoshop's version  of Lorem Ipsum velit auctor aliquet. Aenean sollicitudin lorem quis bibendum auctor, nisi elit 											consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan 										ipsum velit.										
+									</p>
+								</div>
+								<!--proy 1-->
+								
+								
+								<!--proy 1-->
+								<div class="sp center">
+									<img src="img/proy-placeholder.jpg" alt="">
+									<h2>Titulo 03</h2>
+									<p>This is Photoshop's version  of Lorem Ipsum velit auctor aliquet. Aenean sollicitudin lorem quis bibendum auctor, nisi elit 											consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan 										ipsum velit.										
+									</p>
+								</div>
+								<!--proy 1-->
+								
+							</div>	
+							<!--proyectos-->									
+							
+							
+					    <!-- content -->					  </div>
+					  <!--TIC-->
 					  
+					  <!--EPM-->
 					  <div id="epm">
-					    <h2>EPM</h2>
+					    <h2>E P M</h2>
+					    
+					    	<h5 class="border-top-red">
+						    La ciudad es producto de una construcción social. Desde esta perspectiva buscamos promover el enriquecimiento del capital social a través de intervenciones comunitarias que contribuyan a la mejora del entorno y el fortalecimiento de la identidad.
+					    	</h5>
+					    	
+					    	<p>Todo proyecto urbano de alto impacto social debe contemplar la participación activa de su comunidad.<br>
+					    	<br>
+En Cuadra Urbanismo diseñamos plataformas de co-creación a partir de la sensibilización social y la aplicación de métodos etnográficos. El objetivo es acercar a los tomadores de decisiones con los ciudadanos y lograr así procesos democráticos de transformación urbana.</p>
+
+							<ul>
+								<li><span class="red-square"></span> Plataformas de co-creación de    proyectos (socialización)</li>
+								<li><span class="red-square"></span> Activaciones urbanas     (placemaking)</li>
+								<li><span class="red-square"></span> Estrategias de sensibilización y  comunicación de  proyectos urbanos</li>
+								<li><span class="red-square"></span> Rehabilitación colaborativa de barrios (Huertos urbanos, murales, gestión cultural, etc.)</li>
+							</ul>	
+							
+							<div class="clear"></div>									
+							<!--proyectos-->
+							<div class="serv-proyectos">
+								
+								<!--proy 1-->
+								<div class="sp center">
+									<img src="img/proy-placeholder.jpg" alt="">
+									<h2>Titulo 01</h2>
+									<p>This is Photoshop's version  of Lorem Ipsum velit auctor aliquet. Aenean sollicitudin lorem quis bibendum auctor, nisi elit 											consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan 										ipsum velit.										
+									</p>
+								</div>
+								<!--proy 2-->
+								
+								<!--proy 1-->
+								<div class="sp center">
+									<img src="img/proy-placeholder.jpg" alt="">
+									<h2>Titulo 02</h2>
+									<p>This is Photoshop's version  of Lorem Ipsum velit auctor aliquet. Aenean sollicitudin lorem quis bibendum auctor, nisi elit 											consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan 										ipsum velit.										
+									</p>
+								</div>
+								<!--proy 1-->
+								
+								
+								<!--proy 1-->
+								<div class="sp center">
+									<img src="img/proy-placeholder.jpg" alt="">
+									<h2>Titulo 03</h2>
+									<p>This is Photoshop's version  of Lorem Ipsum velit auctor aliquet. Aenean sollicitudin lorem quis bibendum auctor, nisi elit 											consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan 										ipsum velit.										
+									</p>
+								</div>
+								<!--proy 1-->
+								
+							</div>	
+							<!--proyectos-->									
+							
+							
 					    <!-- content -->
 					  </div>
+					  <!--EPM-->
 					  
+					  <!--PP-->
 					  <div id="pp">
-					    <h2>PP</h2>
+					    <h2>P P</h2>
+					    
+					    	<h5 class="border-top-red">
+						    La ciudad es producto de una construcción social. Desde esta perspectiva buscamos promover el enriquecimiento del capital social a través de intervenciones comunitarias que contribuyan a la mejora del entorno y el fortalecimiento de la identidad.
+					    	</h5>
+					    	
+					    	<p>Todo proyecto urbano de alto impacto social debe contemplar la participación activa de su comunidad.<br>
+					    	<br>
+En Cuadra Urbanismo diseñamos plataformas de co-creación a partir de la sensibilización social y la aplicación de métodos etnográficos. El objetivo es acercar a los tomadores de decisiones con los ciudadanos y lograr así procesos democráticos de transformación urbana.</p>
+
+							<ul>
+								<li><span class="red-square"></span> Plataformas de co-creación de    proyectos (socialización)</li>
+								<li><span class="red-square"></span> Activaciones urbanas     (placemaking)</li>
+								<li><span class="red-square"></span> Estrategias de sensibilización y  comunicación de  proyectos urbanos</li>
+								<li><span class="red-square"></span> Rehabilitación colaborativa de barrios (Huertos urbanos, murales, gestión cultural, etc.)</li>
+							</ul>	
+							
+							<div class="clear"></div>									
+							<!--proyectos-->
+							<div class="serv-proyectos">
+								
+								<!--proy 1-->
+								<div class="sp center">
+									<img src="img/proy-placeholder.jpg" alt="">
+									<h2>Titulo 01</h2>
+									<p>This is Photoshop's version  of Lorem Ipsum velit auctor aliquet. Aenean sollicitudin lorem quis bibendum auctor, nisi elit 											consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan 										ipsum velit.										
+									</p>
+								</div>
+								<!--proy 2-->
+								
+								<!--proy 1-->
+								<div class="sp center">
+									<img src="img/proy-placeholder.jpg" alt="">
+									<h2>Titulo 02</h2>
+									<p>This is Photoshop's version  of Lorem Ipsum velit auctor aliquet. Aenean sollicitudin lorem quis bibendum auctor, nisi elit 											consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan 										ipsum velit.										
+									</p>
+								</div>
+								<!--proy 1-->
+								
+								
+								<!--proy 1-->
+								<div class="sp center">
+									<img src="img/proy-placeholder.jpg" alt="">
+									<h2>Titulo 03</h2>
+									<p>This is Photoshop's version  of Lorem Ipsum velit auctor aliquet. Aenean sollicitudin lorem quis bibendum auctor, nisi elit 											consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan 										ipsum velit.										
+									</p>
+								</div>
+								<!--proy 1-->
+								
+							</div>	
+							<!--proyectos-->									
+							
+							
 					    <!-- content -->
 					  </div>
+					  <!--PP-->
+					  
 				</div> 
 			</div>
 		</div>
