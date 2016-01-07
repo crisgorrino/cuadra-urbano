0642e558f31b93fac7bdf9f5692e33b3080e6069
Cristian Gorrino <cristiangorrino@Cristians-MacBook-Air.local>
2016-01-06 20:16:52 -0600

    proyecto detalle machote

---
+++ detalle-proyecto.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+  <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Cuadra Urbano - Proyecto - Barrios Amables</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="styles/main.css">
+
+    </head>
+    <body class="proyectos">
+		<!--header include-->
+		<?php include('includes/header.php') ?>
+		<!--header include-->
+		
+		<div class="mast">
+			<img src="img/proyectos/barrios_amables/barios_amables_main.jpg" alt="cuadra urbano">
+		</div>
+		
+		
+		<div class="container bg-gray">
+			<div class="inner cf ">
+				<div class="left">
+					<h1 class="left">Barrios Amables</h1>
+					<p class="clear gray">Barrios Amables Año: 2012 - 2015 — Cliente: Ayuntamiento de Zapopan</p>
+				</div>	
+				<ul class='proy-filters'>
+					<li class='filter active'><a href="#us">US</a></li>
+					<li class='filter active'><a href="#ui">UI</a></li>
+					<li class='filter'><a href="#tic">TIC</a></li>
+					<li class='filter'><a href="#epm">EPM</a></li>
+					<li class='filter'><a href="#pp">PP</a></li>
+				</ul>
+				<div class="clear"></div>
+					<!--proyectos-->
+					<div class="detalle-proyectos">
+								
+					<p>Barrios Amables es un programa social del Gobierno del municipio de Zapopan, México, en el cual Cuadra Urbanismo participa como consultor en el análisis de las dinámicas sociales y urbanas. Se generan metodologías de intervención de carácter social y se realizan talleres de recuperación y apropiación del espacio público. El objetivo principal de programa es generar comunidades autogestivas mediante el empoderamiento de los ciudadanosy la formación de elementos de identidad y equidad.El programa se centra en el desarrollo de comunidades marginadas en el que Cuadra Urbanismo interviene en torno a cinco líneas estratégicas: la expresión, la sensibilización, la comunicación, la identidad y el vínculo con los diferentes actores.
+					</p>	
+					
+					<div class="slider">
+						<img src="img/proyectos/barrios_amables/slider-placeholder.jpg" alt="">
+					</div>	
+								
+					</div>	
+					<!--proyectos-->						
+			</div>
+		</div>
+				
+    </body>
+</html>
